206c70883e0cfe71f1179b151d1e5738c0e92094
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2023-08-05 23:17:56 +0300

    q

---
+++ src/js/theme.js
@@ -0,0 +1,48 @@
+const bodyEl = document.querySelector('body');
+bodyEl.style.backgroundColor = localStorage.getItem('themeColor');
+
+export function onChangeTheme() {
+  const categoriesEl = document.querySelectorAll(
+    '.white-theme-color-Allcategories'
+  );
+  const mainTextEl = document.querySelectorAll('.white-theme-color-main-text');
+  const modalEl = document.querySelectorAll('.white-theme-color-modal');
+  const svgEl = document.querySelectorAll('.white-theme-color-svg');
+  if (bodyEl.style.backgroundColor === 'black') {
+    localStorage.setItem(
+      'themeColor',
+      (bodyEl.style.backgroundColor = 'white')
+    );
+    bodyEl.style.backgroundColor = localStorage.getItem('themeColor');
+    categoriesEl.forEach(item => {
+      item.classList.remove('black-theme');
+    });
+    mainTextEl.forEach(item => {
+      item.classList.remove('black-theme');
+    });
+    modalEl.forEach(item => {
+      item.classList.remove('black-theme');
+    });
+    svgEl.forEach(item => {
+      item.classList.remove('black-theme');
+    });
+  } else {
+    localStorage.setItem(
+      'themeColor',
+      (bodyEl.style.backgroundColor = 'black')
+    );
+    bodyEl.style.backgroundColor = localStorage.getItem('themeColor');
+    categoriesEl.forEach(item => {
+      item.classList.add('black-theme');
+    });
+    mainTextEl.forEach(item => {
+      item.classList.add('black-theme');
+    });
+    modalEl.forEach(item => {
+      item.classList.add('black-theme');
+    });
+    svgEl.forEach(item => {
+      item.classList.add('black-theme');
+    });
+  }
+}
