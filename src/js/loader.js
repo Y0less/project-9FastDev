15f562e71b860c6dc676b7d5e7934ad0eaca7843
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-08-06 22:50:02 +0300

    partial

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,7 +1,18 @@
 const loader = document.querySelector('.loader-container');
-
+const dialog = document.querySelector('dialog');
+let state = false;
 export default function loadingToogle() {
-  loader.classList.toggle('loader-hidden');
+  // loader.classList.toggle('loader-hidden');
+  if (!state) {
+    dialog.showModal();
+    state = true;
+    return;
+  }
+  if (state) {
+    dialog.close();
+    state = false;
+    return;
+  }
 }
 
 // console.log('Hello Loader!');
