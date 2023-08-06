0857867722ec88ef7a51bb07c0c5fb5bb4b0629c
Vadym-Okhrymenko <vadimliongroup@gmail.com>
2023-08-06 19:00:43 +0300

    v.3

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,12 @@
 import { save, load, remove } from './storage';
 
 console.log('Hello Header!');
+
+const burgerMenu = document.querySelector('.burger-menu');
+
+const overlay = document.querySelector('.menu');
+
+burgerMenu.addEventListener('click', function () {
+  this.classList.toggle('close');
+  overlay.classList.toggle('overlay');
+});
