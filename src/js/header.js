42c09fa5f8f792a6d42194dbad305ee3b79884f4
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-08-08 00:19:51 +0300

    some editing

--- src/js/header.js
+++ src/js/header.js
@@ -3,18 +3,17 @@ import { save, load, remove } from './storage';
 console.log('Hello Header!');
 
 // Функція для відкриття/
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-header-modal]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-header-modal]'),
+};
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-    refs.openModalBtn.classList.toggle('is-close');
-  }
-})();
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-open');
+  refs.openModalBtn.classList.toggle('is-close');
+}
