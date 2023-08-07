5a16ff9005518cda2116106a02354f87cbeb52c0
Vadym-Okhrymenko <vadimliongroup@gmail.com>
2023-08-08 01:30:28 +0300

    v.1
    add class "active-page" on active link

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,37 @@
 import { save, load, remove } from './storage';
-
-console.log('Hello Header!');
+import { save, load, remove } from './storage';
 
 // Функція для відкриття/
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-header-modal]'),
+  };
 
-const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
-  modal: document.querySelector('[data-header-modal]'),
-};
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+    refs.openModalBtn.classList.toggle('is-close');
+  }
+})();
 
-function toggleModal() {
-  refs.modal.classList.toggle('is-open');
-  refs.openModalBtn.classList.toggle('is-close');
+// Функція для active-page
+
+function setActiveLink() {
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.classList.remove('active-page');
+  });
+
+  const currentURL = window.location.href;
+
+  links.forEach(link => {
+    if (link.href === currentURL) {
+      link.classList.add('active-page');
+    }
+  });
 }
+window.addEventListener('load', setActiveLink);
