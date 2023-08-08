efae948e18ff105a34924e4db3f42c8d470d6cab
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-08-08 21:30:05 +0300

    bugfix/active-page

--- src/js/header.js
+++ src/js/header.js
@@ -2,36 +2,31 @@ import { save, load, remove } from './storage';
 import { save, load, remove } from './storage';
 
 // Функція для відкриття/
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-header-modal]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-header-modal]'),
+};
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-    refs.openModalBtn.classList.toggle('is-close');
-  }
-})();
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-open');
+  refs.openModalBtn.classList.toggle('is-close');
+}
 
 // Функція для active-page
+const currentURL = window.location.href;
+const links = document.querySelectorAll('nav a');
 
 function setActiveLink() {
-  const links = document.querySelectorAll('nav a');
-  links.forEach(link => {
-    link.classList.remove('active-page');
-  });
-
-  const currentURL = window.location.href;
-
   links.forEach(link => {
     if (link.href === currentURL) {
       link.classList.add('active-page');
     }
   });
 }
-window.addEventListener('load', setActiveLink);
+// window.addEventListener('load', setActiveLink);
+setActiveLink();
