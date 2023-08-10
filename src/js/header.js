318230eff5f13b499622204b7de831b3738bf24a
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-08-10 07:59:41 +0300

    fixing close button

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,22 @@
 import { save, load, remove } from './storage';
 
 // Функція для відкриття/
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-header-modal]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeMobileModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-header-modal]'),
+};
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-    refs.openModalBtn.classList.toggle('is-close');
-  }
-})();
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeMobileModalBtn.addEventListener('click', toggleModal);
 
+function toggleModal() {
+  refs.modal.classList.toggle('is-open');
+  refs.openModalBtn.classList.toggle('is-close');
+  refs.closeMobileModalBtn.classList.toggle('visually-hidden');
+}
+console.log('refs.closeMobileModalBtn :>> ', refs.closeMobileModalBtn);
 // Функція для active-page
 
 function setActiveLink() {
