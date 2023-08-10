9a0de1620f310e92d177aeee494afe97b930a56b
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-08-10 08:44:32 +0300

    implementing scrolllock

--- src/js/header.js
+++ src/js/header.js
@@ -6,17 +6,24 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeMobileModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-header-modal]'),
+  pageBody: document.querySelector('body'),
 };
+
+console.log('pageBody :>> ', refs.pageBody);
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeMobileModalBtn.addEventListener('click', toggleModal);
 
-function toggleModal() {
+function toggleModal(e) {
+  refs.pageBody.classList.toggle('disable-scroll');
   refs.modal.classList.toggle('is-open');
   refs.openModalBtn.classList.toggle('is-close');
   refs.closeMobileModalBtn.classList.toggle('visually-hidden');
 }
-console.log('refs.closeMobileModalBtn :>> ', refs.closeMobileModalBtn);
+
+document.body.scroll = 'no';
+// function disableScroll(params) {}
+
 // Функція для active-page
 
 function setActiveLink() {
